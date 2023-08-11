94af4a19e63ed3e80f8012e183ad0b82d2a45639
OlajummyO <olajummy.olajumoke@gmail.com>
2023-08-11 15:28:26 -0700

    changes

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,20 +1,30 @@
 #!/usr/bin/python3
+import sys
+from calculator_1 import add, subtract, multiply, divide
 
-if __name__ == "__main__":
-    """Handle basic arithmetic operations."""
-    from calculator_1 import add, sub, mul, div 
-    import sys
-
-    if len(sys.argv) - 1 != 3:
+def main():
+    if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-    ops = {"+": add, "-": sub, "*": mul, "/": div}
-    if sys.argv[2] not in list(ops.keys()):
+    a = int(sys.argv[1])
+    operator = sys.argv[2]
+    b = int(sys.argv[3])
+
+    if operator == '+':
+        result = add(a, b)
+    elif operator == '-':
+        result = subtract(a, b)
+    elif operator == '*':
+        result = multiply(a, b)
+    elif operator == '/':
+        result = divide(a, b)
+    else:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
-    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
+    print("{} {} {} = {}".format(a, operator, b, result))
+
+if __name__ == "__main__":
+    main()
 
